c571f4b71085ee78f76dca55e16c3fd97303e6c3
Ignacio Lopezosa Serrano <ignacio.lopezosa@ebury.com>
2022-09-09 12:28:38 +0200

    if __name__ == "__main__":

--- BotPublisherTemplate.py
+++ BotPublisherTemplate.py
@@ -16,57 +16,59 @@ DESCRIPCION = "AQUI TU TEXTO" + ENLACE
 ACCOUNT = "AQUI TU @"   
 PSWD = "AQUI TU CONTRASENIA"   
 
-# ----------------------------------- #
-# --------- Instagram Stuff --------- #
-# ----------------------------------- #
+# Execution
+if __name__ == "__main__":
+    # ----------------------------------- #
+    # --------- Instagram Stuff --------- #
+    # ----------------------------------- #
 
-image = Image.open(IMAGE)
-image = image.convert("RGB")
-# Es necesario reajustar la imagen
-new_image = image.resize((1080, 1080))
-new_image.save("imagen_reajustada.jpg")
-phot_path = "imagen_reajustada.jpg"
-phot_path = Path(phot_path)
+    image = Image.open(IMAGE)
+    image = image.convert("RGB")
+    # Es necesario reajustar la imagen
+    new_image = image.resize((1080, 1080))
+    new_image.save("imagen_reajustada.jpg")
+    phot_path = "imagen_reajustada.jpg"
+    phot_path = Path(phot_path)
 
-cl = Client()
-# Los settings permiten evitar el reiterado envio al email del codigo de confirmacion
-cl.load_settings('/tmp/dump.json') # Descomentado EXCEPTO en la primera ejecucion
-cl.login(ACCOUNT, PSWD)
-cl.get_timeline_feed()  # Descomentado EXCEPTO en la primera ejecucion
-#cl.dump_settings('/tmp/dump.json') # Descomentado SOLO en la primera ejecucion
+    cl = Client()
+    # Los settings permiten evitar el reiterado envio al email del codigo de confirmacion
+    cl.load_settings('/tmp/dump.json') # Descomentado EXCEPTO en la primera ejecucion
+    cl.login(ACCOUNT, PSWD)
+    cl.get_timeline_feed()  # Descomentado EXCEPTO en la primera ejecucion
+    #cl.dump_settings('/tmp/dump.json') # Descomentado SOLO en la primera ejecucion
 
-print("Comenzando a publicar en Instagram...")
+    print("Comenzando a publicar en Instagram...")
 
-cl.photo_upload(phot_path, DESCRIPCION)
-cl.photo_upload_to_story(phot_path, links=[StoryLink(webUri=ENLACE)])
-# Consulta el objeto cl para mas acciones...
+    cl.photo_upload(phot_path, DESCRIPCION)
+    cl.photo_upload_to_story(phot_path, links=[StoryLink(webUri=ENLACE)])
+    # Consulta el objeto cl para mas acciones...
 
-print("¡Publicacion en Instagram completada!")
+    print("¡Publicacion en Instagram completada!")
 
-# ----------------------------------- #
-# ---------- Twitter Stuff ---------- #
-# ----------------------------------- #
+    # ----------------------------------- #
+    # ---------- Twitter Stuff ---------- #
+    # ----------------------------------- #
 
-# Necesita registro en https://developer.twitter.com/en
-# Recuerda que las claves deben ser de Read and Write
-consumerKey = ""
-consumerSecret = ""
-accessToken = ""
-accessTokenSecret = ""
-BearerToken = ""
+    # Necesita registro en https://developer.twitter.com/en
+    # Recuerda que las claves deben ser de Read and Write
+    consumerKey = ""
+    consumerSecret = ""
+    accessToken = ""
+    accessTokenSecret = ""
+    BearerToken = ""
 
-# Autenticacion en Twitter
-client = tweepy.Client(
-    bearer_token=BearerToken,
-    consumer_key=consumerKey, 
-    consumer_secret=consumerSecret,
-    access_token=accessToken,
-    access_token_secret=accessTokenSecret
-)
+    # Autenticacion en Twitter
+    client = tweepy.Client(
+        bearer_token=BearerToken,
+        consumer_key=consumerKey, 
+        consumer_secret=consumerSecret,
+        access_token=accessToken,
+        access_token_secret=accessTokenSecret
+    )
 
-print("Comenzando a publicar en Twitter...")
+    print("Comenzando a publicar en Twitter...")
 
-client.create_tweet(text=DESCRIPCION)
-# Consulta el objeto client para mas acciones...
+    client.create_tweet(text=DESCRIPCION)
+    # Consulta el objeto client para mas acciones...
 
-print("¡Publicacion en Twitter completada!")
+    print("¡Publicacion en Twitter completada!")
